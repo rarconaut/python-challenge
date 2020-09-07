3bea473a1cffda8dab9cd8a46b4aa3a3b444260a
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-09-07 16:23:17 -0700

    Now trying to create lists for both profits and losses to perform the calculations.

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,16 +12,18 @@ def print_analysis(budgetData):
     # variables to store the data
     date = str(budgetData[0])
     profit_loss = float(budgetData[1])
+    profits = []
+    losses = []
     greatest_inc = 0
     greatest_des = 0
 
 ## reading the budget_data.csv file into Python
 with open(pybank_csv, 'r') as csvfile:
     budgetData = csv.reader(csvfile, delimiter=',')
-
+    
     # defining the header
     header = next(budgetData)
-  
+
    #=========================================# 
        
     # Your task for PyBank is to create a Python script that analyzes the records to calculate each of the 
@@ -35,7 +37,10 @@ with open(pybank_csv, 'r') as csvfile:
     for row in budgetData:
         # The net total amount of "Profit/Losses" over the entire period
         ## net_total = sum(Profits) - sum(Losses)
-        if row > 0:
+        if row[1] > 0:
+            profits.append(row[1])
+        elif row[1] < 0:
+            losses.append(row[1])
             
 
         # The average of the changes in "Profit/Losses" over the entire period
