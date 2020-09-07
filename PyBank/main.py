e6c8fbd66da041f19b4d8b13640b39326e65aaca
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-09-07 12:24:53 -0700

    Looking at wrestling_functions.py for inspiration. Still working on defining variables and if statements for the remaining calculations.

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,12 +10,8 @@ pybank_csv = os.path.join("Resources", "budget_data.csv")
 def print_analysis(budgetData):
 
     # variables to store the data
-    month = budgetData[0]
-    total_months= len(list(budgetData))
-    net_total = float(budgetData[1])
-    avg_change = float()
-    greatest_inc = float()
-    greatest_dec = float()
+    date = str(budgetData[0])
+    profit/loss = float(budgetData[1])
 
 ## reading the budget_data.csv file into Python
 with open(pybank_csv, 'r') as csvfile:
@@ -24,26 +20,27 @@ with open(pybank_csv, 'r') as csvfile:
     # defining the header
     header = next(budgetData)
   
-    
+   #=========================================# 
        
     # Your task for PyBank is to create a Python script that analyzes the records to calculate each of the 
     # following:
 
     # The total number of months included in the dataset
-    
-    print(total_months)
+    ## total months is equal to the number of rows (minus the header)
+    total_months= len(list(budgetData))
+    print(f'Total number of months = {total_months}')
 
     # The net total amount of "Profit/Losses" over the entire period
-
+    ## net_total = Profit - totalLoss
 
     # The average of the changes in "Profit/Losses" over the entire period
-
+    ## avg_change = sum of differences month to month/total months
 
     # The greatest increase in profits (date and amount) over the entire period
-
+    greatest_inc =
 
     # The greatest decrease in losses (date and amount) over the entire period
-
+    greatest_dec =
 
 # In addition, your final script should both print the analysis to the terminal and export a text file 
 # with the results.
