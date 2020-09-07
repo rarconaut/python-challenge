d63fb6d1f627e6553443a6889d3989eba60dea60
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-09-07 11:25:13 -0700

    Trying to define and get the month count working. Having trouble getting the operations/calculations to work outside of the 'with open(pybank_csv) as csvfile'.

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,39 +6,43 @@ import csv
 ## define the csv path
 pybank_csv = os.path.join("Resources", "budget_data.csv")
 
-## define the function and accept 'budget_data' as its parameter
+# define the function and accept 'budget_data' as its parameter
 def print_analysis(budgetData):
 
-    ## variables to store the data
-    months = int(budgetData[0])
+    # variables to store the data
+    month = budgetData[0]
+    total_months= len(list(budgetData))
     net_total = float(budgetData[1])
     avg_change = float()
     greatest_inc = float()
     greatest_dec = float()
 
 ## reading the budget_data.csv file into Python
-with open(pybank_csv, 'r') as csvfile:   
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(pybank_csv, 'r') as csvfile:
+    budgetData = csv.reader(csvfile, delimiter=',')
 
-    for row in csvreader:
+    # defining the header
+    header = next(budgetData)
+  
+    
+       
+    # Your task for PyBank is to create a Python script that analyzes the records to calculate each of the 
+    # following:
 
-# Your task for PyBank is to create a Python script that analyzes the records to calculate each of the 
-# following:
+    # The total number of months included in the dataset
+    
+    print(total_months)
 
-
-# The total number of months included in the dataset
-
-
-# The net total amount of "Profit/Losses" over the entire period
-
-
-# The average of the changes in "Profit/Losses" over the entire period
-
-
-# The greatest increase in profits (date and amount) over the entire period
+    # The net total amount of "Profit/Losses" over the entire period
 
 
-# The greatest decrease in losses (date and amount) over the entire period
+    # The average of the changes in "Profit/Losses" over the entire period
+
+
+    # The greatest increase in profits (date and amount) over the entire period
+
+
+    # The greatest decrease in losses (date and amount) over the entire period
 
 
 # In addition, your final script should both print the analysis to the terminal and export a text file 
